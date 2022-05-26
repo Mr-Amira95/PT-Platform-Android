0e700666093b7977e11d811de483bcc28bbb890f
Mr.Amira <h.amira@qtechnetworks.com>
2022-05-26 15:18:22 +0300

    Add Home Page Icons 26-5-2022

--- app/src/main/java/com/qtechnetworks/ptplatform/View/Fragment/MainFragment.java
+++ app/src/main/java/com/qtechnetworks/ptplatform/View/Fragment/MainFragment.java
@@ -37,8 +37,8 @@ public class MainFragment extends Fragment implements CallBack {
     private SliderAdapter sliderAdapter;
 
     private LinearLayout exercisesLayout, workoutsLayout, nutritionLayout, progressLayout,
-    personalLayout, challengesLayout, favouriteLayout, todayWorkoutLayout, contactUsLayout,
-    shopLayout, videoChatLayout, liveChatLayout;
+    personalLayout, challengesLayout, favouriteLayout, calenderLayout, todayWorkout, contactUsLayout,
+    shopLayout, videoChatLayout, liveChatLayout, historyLayout, kycLayout;
 
     CoachesDialog coachesDialog;
 
@@ -112,7 +112,7 @@ public class MainFragment extends Fragment implements CallBack {
             }
         });
 
-        todayWorkoutLayout.setOnClickListener(new View.OnClickListener() {
+        calenderLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setFragment( new LogFragment("Today’s Workouts"),coachid);
@@ -199,11 +199,15 @@ public class MainFragment extends Fragment implements CallBack {
         personalLayout = view.findViewById(R.id.personal_layout);
         challengesLayout = view.findViewById(R.id.challenges_layout);
         favouriteLayout = view.findViewById(R.id.favourite_layout);
-        todayWorkoutLayout = view.findViewById(R.id.today_workout_layout);
+        calenderLayout = view.findViewById(R.id.calendar_layout);
         contactUsLayout = view.findViewById(R.id.contact_us_layout);
         shopLayout = view.findViewById(R.id.shop_layout);
         videoChatLayout = view.findViewById(R.id.video_chat_layout);
         liveChatLayout = view.findViewById(R.id.live_chat_layout);
+        todayWorkout = view.findViewById(R.id.today_layout);
+        historyLayout = view.findViewById(R.id.history_layout);
+        kycLayout = view.findViewById(R.id.kyc_layout);
+
         cate1_image=view.findViewById(R.id.cate1_image);
 
         coachesDialog = new CoachesDialog(getContext());

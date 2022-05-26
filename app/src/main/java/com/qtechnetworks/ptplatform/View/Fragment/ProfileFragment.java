c265378f90293bffbf7ee541bcc78e31886eb4fe
qtechnetworks <bitbucket@qtechnetworks.com>
2022-05-26 17:37:39 +0300

    initial

--- app/src/main/java/com/qtechnetworks/ptplatform/View/Fragment/ProfileFragment.java
+++ app/src/main/java/com/qtechnetworks/ptplatform/View/Fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.qtechnetworks.ptplatform.View.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,68 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.qtechnetworks.ptplatform.Model.utilits.PreferencesUtils;
 import com.qtechnetworks.ptplatform.R;
+import com.qtechnetworks.ptplatform.View.Activity.ChoosingActivity;
 
 public class ProfileFragment extends Fragment {
+
+    TextView username,joining_date;
+
+    RoundedImageView profile_img;
+
+    TextView assigned_coaches,progress,subscriptions,kyc,settings,logout;
+
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        view=inflater.inflate(R.layout.fragment_profile, container, false);
+
+        initial(view);
+
+        return view;
     }
+
+    private void initial(View v){
+        username=v.findViewById(R.id.username);
+        joining_date=v.findViewById(R.id.joining_date);
+        profile_img=v.findViewById(R.id.profile_img);
+
+        assigned_coaches=v.findViewById(R.id.assigned_coaches);
+        progress=v.findViewById(R.id.progress);
+        subscriptions=v.findViewById(R.id.subscriptions);
+        kyc=v.findViewById(R.id.kyc);
+        settings=v.findViewById(R.id.settings);
+        logout=v.findViewById(R.id.logout);
+
+        try{
+            Glide.with(getContext()).load(PreferencesUtils.getUser(getContext()).getAvatar()).placeholder(R.drawable.logo).into(profile_img);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PreferencesUtils.clearDefaults(getContext());
+                startActivity(new Intent(getContext(), ChoosingActivity.class));
+                getActivity().finish();
+
+            }
+        });
+
+        joining_date.setText(PreferencesUtils.getUser(getContext()).getEmail());
+
+        username.setText(PreferencesUtils.getUser(getContext()).getFirstName()+" "+PreferencesUtils.getUser(getContext()).getLastName());
+
+    }
+
 }
